6ceabbd7c89985d793eab77357ba9ee7adce7ee3
YEdirisingha <yasinduedirisingha@gmail.com>
2023-12-17 21:02:49 +0530

    compleated BillGenerate class

--- burgerShop.java
+++ burgerShop.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
-class burger{
+class Burger{
     private String name;
     private double price;
 
-    public burger(String name,double price){
+    public Burger(String name,double price){
         this.name=name;
         this.price=price;
     }
@@ -16,27 +16,34 @@ class burger{
         return name+" Rs. "+price;
     }
 }
-class billGenerate{
+class BillGenerate{
     private List<Burger> order;
 
-    public billGenerate(){
+    public BillGenerate(){
         order =new ArrayList<>();
     } 
-    public void addBurger(){
-
+    public void addBurger(Burger burger){
+        order.add(burger);
     }
     public double calculateTotal()
     {
-
+        double total=0;
+        for(Burger burger:order){
+            total +=burger.getPrice();
+        }
+        return total;
     }
     public void displayOrder()
     {
-
+        System.out.println("Your order: ");
+        for(Burger burger:order){
+            System.out.println(burger);
+        }
+        System.out.println("Total is : Rs. "+calculateTotal());
     }
 }
 public class burgerShop {
     public static void main(String args[]){
-        
-        
+
     }
 }

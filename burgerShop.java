713a7eff0f10f9555b8c432c240eb4fd25a9c227
YEdirisingha <yasinduedirisingha@gmail.com>
2023-12-17 18:50:04 +0530

    creating variables and methods for burger class

--- burgerShop.java
+++ burgerShop.java
@@ -1,5 +1,17 @@
 public class burger{
+    private String name;
+    private double price;
 
+    public burger(String name,double price){
+        this.name=name;
+        this.price=price;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public String getName(){
+        return name+" Rs. "+price;
+    }
 }
 public class billGenerate{
     
